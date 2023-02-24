bdee7176cf55dc1ea70bda89ce47394deb3ccf9e
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2023-02-24 13:55:34 +0100

    remove Publishing from fps print..

--- frameset-to-background-image/src/background_image_node.cpp
+++ frameset-to-background-image/src/background_image_node.cpp
@@ -366,7 +366,7 @@ void BackgroundImageNode::PrintFPS(const float fps, const float itrTime)
   	}
 
 	if (m_print_fps)
-		RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+		RCLCPP_INFO(this->get_logger(), message.data.c_str());
 }
 
 
